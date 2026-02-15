ffa742bccbf9a4cba27de4d3da3d93cf1d2f6324
Hazem Gamal <hazemgamal223311@gmail.com>
2026-02-15 03:35:42 +0200

    Add dotenv support and implement IdeaValidationAgent class

---
+++ subagents/IdeaValidationAgent.py
@@ -0,0 +1,24 @@
+from langchain.agents import create_agent 
+from langchain.messages import HumanMessage,SystemMessage
+from langchain.tools import tool
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+class IdeaValidationAgent:
+    def __init__(self):
+        self.system_prompt=SystemMessage("")
+
+
+
+    
+    
+
+
+
+
+
+
+
