ef62fa74d1c0153b4d76cb43a0379dfc1b8e7576
Hazem Gamal <hazemgamal223311@gmail.com>
2026-02-18 10:07:32 +0200

    Refactor code structure for improved readability and maintainability

--- subagents/test_rag.py
+++ subagents/test_rag.py
@@ -1,26 +1,21 @@
-import chromadb
-from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Use PersistentClient for on-disk storage
-client = chromadb.PersistentClient(path="./chroma_db")
-
-embedding_function = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-
-# get_or_create_collection avoids errors on re-runs
-collection = client.get_or_create_collection(
-    name="docs", embedding_function=embedding_function
-)
-
-collection.add(
-    ids=["1", "2", "3","4"],
-    documents=[
-        "Chroma is an open-source vector database",
-        "Vector databases store embeddings",
-        "RAG uses vector search with LLMs",
-        "my name is hazem",
-    ],
-)
-
-results = collection.query(query_texts=["What is your name ?"], n_results=1)
-
-print(results["documents"])
+if __name__ == "__main__":
+    file_path = r"E:\Langchain-projects\StartAI\subagents\Hazem_Gamal_CV_.pdf"
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    all_splits = text_splitter.split_documents(docs)
+    embedding_function = OllamaEmbeddings(model="nomic-embed-text:latest")
+    vector_store = Chroma(
+        "my_first_collection",
+        embedding_function=embedding_function,
+        persist_directory="./chroma_langchain_db",
+    )
+    vector_store.add_documents(documents=all_splits)
+    print(vector_store.similarity_search("what is my GPA ?", k=1))
